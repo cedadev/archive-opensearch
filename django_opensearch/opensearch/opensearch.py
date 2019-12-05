97c026a67619f6ec6e1731d9680280fd6e060530
Peter Marek <peter.marek@stfc.ac.uk>
2019-12-05 10:06:35 +0000

    fix #14

--- django_opensearch/opensearch/opensearch.py
+++ django_opensearch/opensearch/opensearch.py
@@ -125,9 +125,6 @@ class OpensearchResponse:
 
         if self.totalResults > self.itemsPerPage:
             # Generate paging links
-            print('yyyyy', self.totalResults / self.itemsPerPage)
-            print('yyyyyy', self.startPage + 0.5)
-            print('yyyyy', self.startPage)
             if self.startPage > 1:
 
                 self.links['first'] = [
@@ -144,9 +141,6 @@ class OpensearchResponse:
                     ]
 
             if self.startPage < self.totalResults / self.itemsPerPage:
-                print('sssssss', self.totalResults / self.itemsPerPage)
-                print('ssssssss', self.startPage + 0.5)
-                print('ssssssss', self.startPage)
                 self.links['next'] = [
                         {
                             'href': self._generate_navigation_url(full_uri, search_params, 'next'),
