36ac6194663ed8a9455bd6651f21cfe1dd6e6243
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-08-09 08:45:37 +0100

    Markdown to views docstrings

--- events/serialisers.py
+++ events/serialisers.py
@@ -8,12 +8,14 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer class to transform JSON or list of JSON to django model.
 
     Expected format produced from script:
+    ```
     {
         "collection_id": String, max_length=50
         "collection_title": String, max_length=1024
         "action": Choice of ['added', 'updated', 'removed']
         "datetime": ISO-8601 datetime format
     }
+    ```
     """
 
     datetime = fields.DateField(input_formats=["%Y-%m-%dT%H:%M:%S.%f"])
