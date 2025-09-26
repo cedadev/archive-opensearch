024db9d3ca6b1f5fc2a6a030babbbcb6a9832d22
rhysrevans3 <rhys.r.evans3@gmail.com>
2025-09-26 15:36:04 +0100

    Adding openEO service.

--- ceda_opensearch/opensearch_settings.py
+++ ceda_opensearch/opensearch_settings.py
@@ -2,17 +2,19 @@
 """
 Constants for the specific opensearch implementation
 """
-__author__ = 'Richard Smith'
-__date__ = '20 Mar 2019'
-__copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
-__license__ = 'BSD - see LICENSE file in top-level package directory'
-__contact__ = 'richard.d.smith@stfc.ac.uk'
+__author__ = "Richard Smith"
+__date__ = "20 Mar 2019"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "richard.d.smith@stfc.ac.uk"
 
 import os
+
 import yaml
-from elasticsearch import Elasticsearch
 from cci_tag_scanner.facets import Facets
 from cci_tag_scanner.utils.elasticsearch import es_connection_kwargs
+from elasticsearch import Elasticsearch
+
 
 def get_from_conf(env):
     """
@@ -20,48 +22,50 @@ def get_from_conf(env):
     if present.
     """
 
-    deploy_settings = '/etc/django/settings.d/20-runtime-settings.yaml'
+    deploy_settings = "/etc/django/settings.d/20-runtime-settings.yaml"
 
     if not os.path.isfile(deploy_settings):
         return None
-    
+
     with open(deploy_settings) as f:
         conf = yaml.safe_load(f)
     try:
-        for prop in env.split('.'):
-            conf = conf.get(prop,{})
+        for prop in env.split("."):
+            conf = conf.get(prop, {})
         return conf
     except:
-        raise ValueError('Invalid config provided')
+        raise ValueError("Invalid config provided")
 
-SHORT_NAME = 'CEDA Opensearch'
-LONG_NAME = 'CEDA Opensearch'
-DESCRIPTION = 'Opensearch interface to the CEDA archive'
-TAGS = ['CEDA','NERC']
-DEVELOPER = 'CEDA'
 
-ELASTICSEARCH_INDEX = get_from_conf('elasticsearch.index') or 'opensearch-files'
-ELASTICSEARCH_COLLECTION_INDEX = 'opensearch-collections'
-APPLICATION_ID = 'opensearch'
-ELASTICSEARCH_CONNECTION_PARAMS = {
-    'timeout': 30
-}
-ELASTICSEARCH_HOSTS=['https://elasticsearch.ceda.ac.uk']
+SHORT_NAME = "CEDA Opensearch"
+LONG_NAME = "CEDA Opensearch"
+DESCRIPTION = "Opensearch interface to the CEDA archive"
+TAGS = ["CEDA", "NERC"]
+DEVELOPER = "CEDA"
+
+ELASTICSEARCH_INDEX = get_from_conf("elasticsearch.index") or "opensearch-files"
+ELASTICSEARCH_COLLECTION_INDEX = "opensearch-collections"
+APPLICATION_ID = "opensearch"
+ELASTICSEARCH_CONNECTION_PARAMS = {"timeout": 30}
+ELASTICSEARCH_HOSTS = ["https://elasticsearch.ceda.ac.uk"]
+
+OPENEO_HOST = "https://api.stac.164.30.69.113.nip.io"
 
 READ_FROM_VOCAB = False
 
-THREDDS_HOST = 'https://data.cci.ceda.ac.uk'
+THREDDS_HOST = "https://data.cci.ceda.ac.uk"
 
-SOLR_HOST = 'https://esgf-index1.ceda.ac.uk/solr'
-SOLR_CORE = 'datasets'
+SOLR_HOST = "https://esgf-index1.ceda.ac.uk/solr"
+SOLR_CORE = "datasets"
 
-OPENSEARCH_BACKEND = 'elasticsearch'
+OPENSEARCH_BACKEND = "elasticsearch"
 
-DATA_BRIDGE_URL = 'https://eo-data-bridge.ceda.ac.uk'
+DATA_BRIDGE_URL = "https://eo-data-bridge.ceda.ac.uk"
 
-EXTERNAL_DATA_SOURCES = ['https://wui.cmsaf.eu/s']
+EXTERNAL_DATA_SOURCES = ["https://wui.cmsaf.eu/s"]
 
 FACETS = Facets()
+
 
 class ElasticsearchConnection:
     """
@@ -76,11 +80,7 @@ class ElasticsearchConnection:
         self.index = index
         self.collection_index = ELASTICSEARCH_COLLECTION_INDEX
         self.es = Elasticsearch(
-            **es_connection_kwargs(
-                hosts=ELASTICSEARCH_HOSTS,
-                api_key=api_key,
-                **ELASTICSEARCH_CONNECTION_PARAMS
-            )
+            **es_connection_kwargs(hosts=ELASTICSEARCH_HOSTS, api_key=api_key, **ELASTICSEARCH_CONNECTION_PARAMS)
         )
 
     def search(self, query):
--- django_opensearch/opensearch/utils/aggregation_tools.py
+++ django_opensearch/opensearch/utils/aggregation_tools.py
@@ -1,25 +1,26 @@
 # encoding: utf-8
-"""
+""" """
+__author__ = "Richard Smith"
+__date__ = "23 Mar 2020"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "richard.d.smith@stfc.ac.uk"
 
-"""
-__author__ = 'Richard Smith'
-__date__ = '23 Mar 2020'
-__copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
-__license__ = 'BSD - see LICENSE file in top-level package directory'
-__contact__ = 'richard.d.smith@stfc.ac.uk'
-
-from django.conf import settings
-from django_opensearch.constants import SUFFIX_MAP
-from django_opensearch.opensearch.utils import thredds_path
 import urllib.parse
 
+from django.conf import settings
 
-def get_thredds_aggregation(id, format='xml'):
-    return f'{settings.THREDDS_HOST}/thredds/datasets/{id}.{format}?dataset={id}'
+from ceda_opensearch import opensearch_settings
+from django_opensearch.constants import SUFFIX_MAP
+from django_opensearch.opensearch.utils import thredds_path
+
+
+def get_thredds_aggregation(id, format="xml"):
+    return f"{settings.THREDDS_HOST}/thredds/datasets/{id}.{format}?dataset={id}"
 
 
 def get_aggregation_search_link(base_url, collection_id, aggregation_id, format):
-    return f'{base_url}/request?parentIdentifier={collection_id}&drsId={aggregation_id}&httpAccept={urllib.parse.quote(format)}'
+    return f"{base_url}/request?parentIdentifier={collection_id}&drsId={aggregation_id}&httpAccept={urllib.parse.quote(format)}"
 
 
 def get_aggregation_capabilities(agg_dict):
@@ -30,10 +31,21 @@ def get_aggregation_capabilities(agg_dict):
     """
     services = []
 
-    for service in agg_dict['services']:
-        services.append({
-            'title': service,
-            'href': f'{thredds_path(service,"")}/{agg_dict["id"]}{SUFFIX_MAP.get(service,"")}'
-        })
+    for service in agg_dict["services"]:
+        if service == "openeo":
+            services.append(
+                {
+                    "title": service,
+                    "href": f'{opensearch_settings.OPENEO_HOST}/collections/{agg_dict["id"].lower()}.openeo',
+                }
+            )
+
+        else:
+            services.append(
+                {
+                    "title": service,
+                    "href": f'{thredds_path(service,"")}/{agg_dict["id"]}{SUFFIX_MAP.get(service,"")}',
+                }
+            )
 
     return services
